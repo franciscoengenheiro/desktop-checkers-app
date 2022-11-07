b23763b09469b49bb69e91bdb52c2885f6c0d0e8
Francisco Enhenheiro <A49428@alunos.isel.pt>
2022-11-07 12:22:17 +0000

    Added companion objects to plant UML design. Introduced fold instead of foreach in AuxFunctions.kt

--- checkers-model/src/test/kotlin/isel/leic/tds/checkers/AuxFunctions.kt
+++ checkers-model/src/test/kotlin/isel/leic/tds/checkers/AuxFunctions.kt
@@ -6,11 +6,10 @@ import isel.leic.tds.checkers.model.*
  * Receives a variable amount of strings, each one indicating a square, and returns a list
  * of all given squares.
  */
-fun squaresToListOf(vararg s: String): List<Square> {
-    var list = emptyList<Square>() // fold?
-    s.forEach { list += validateSqr(it) }
-    return list
-}
+fun squaresToListOf(vararg s: String) =
+    s.fold(listOf<Square>()) { list, sqr ->
+        list + validateSqr(sqr)
+    }
 
 /**
  * Retrieves the amount of regular checkers each player has at the start of the game,
@@ -24,18 +23,15 @@ fun getPlayerPieces(dim: Int): Int = dim/2 * (dim/2 - 1)
  * represent are not valid then throws [IllegalArgumentException].
  * @return A new board updated with all the plays made.
  */
-fun Board.plays(vararg s: String): Board {
+fun Board.plays(vararg s: String) =
     // Format expected example: "<Square1> <Square2>"
-    var b = this
-    s.forEach { // fold?
-        val list = it.split(" ")
+    s.fold(this) { board, string ->
+        val list = string.split(" ")
         require(list.size == 2) { "Incorrect expected arguments" }
         val toSqr = validateSqr(list[0])
         val fromSqr = validateSqr(list[1])
-        b = b.play(toSqr, fromSqr)
+        board.play(toSqr, fromSqr)
     }
-    return b
-}
 
 /**
  * Validates if a string corresponds to a valid Square or throws
